dd25ac615903f9e198e7bd1c7857dfb7b85ecde4
Unknown <kotilainen.jussi@gmail.com>
2017-09-28 18:53:58 +0300

    Better user list

--- src/cljs/clojure_twitter_clone/pages/admin.cljs
+++ src/cljs/clojure_twitter_clone/pages/admin.cljs
@@ -7,32 +7,43 @@
    {:headers {"Accept" "application/transit+json"}
     :handler #(reset! result %)}))
 
-(defn button [text on-click]
-  [:input {
-    :type "button"
-    :value text
-    :on-click on-click}])
+(defn add-user [users]
+  (conj users {:first_name nil :last_name nil :username nil :is_active false}))
 
-(defn user [{:keys [first_name last_name username]}]
+(defn user [{:keys [username
+                    first_name
+                    last_name
+                    email
+                    is_admin
+                    is_active
+                    password_strength]}]
   [:tr
+    [:td username]
     [:td first_name]
     [:td last_name]
-    [:td username]
-    [:td
-      [button "Edit" #(.log js/console "Clicked")]]])
+    [:td email]
+    [:td (if is_admin [:i.glyphicon.glyphicon-ok])]
+    [:td (if is_active [:i.glyphicon.glyphicon-ok])]
+    [:td password_strength]]) ; label
 
 (defn admin-page []
   (let [users (r/atom nil)]
     (fetch-users users)
     (fn []
+      [:div.container
+        [:button {:on-click #(.log js/console "Clicked add")}
+          [:i.glyphicon.glyphicon-plus]]
         [:table.table
           [:thead
             [:tr
+              [:td "Username"]
               [:td "First name"]
               [:td "Last name"]
-              [:td "Username"]
-              [:td]]]
+              [:td "Email"]
+              [:td "Is admin"]
+              [:td "Is active"]
+              [:td "Password"]]]
           [:tbody
             (for [u @users]
               ^{:key (:id u)}
-              [user u])]])))
+              [user u])]]])))
