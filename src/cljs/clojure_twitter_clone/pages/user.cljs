4370ec38a77b3233434bcfdaeac85d1228ceaff2
Unknown <kotilainen.jussi@gmail.com>
2017-11-09 18:43:45 +0200

    Add front component for posting tweets

--- src/cljs/clojure_twitter_clone/pages/user.cljs
+++ src/cljs/clojure_twitter_clone/pages/user.cljs
@@ -1,23 +1,41 @@
 (ns clojure-twitter-clone.pages.user
   (:require [reagent.core :as r]
-            [reagent.session :as session]
             [ajax.core :refer [GET POST]]
-            [clojure-twitter-clone.components.tweet :as tweet]))
+            [clojure-twitter-clone.components.tweet :as tweet]
+            [clojure-twitter-clone.utils.url :as url]
+            [clojure-twitter-clone.components.input :as input]))
 
 (defn fetch-user-tweets [username result]
  (GET (str "/api/" username)
    {:headers {"Accept" "application/transit+json"}
     :handler #(reset! result %)}))
 
+(defn create-tweet [username tweet]
+ (POST (str "/api/" username)
+   {:params tweet}))
+
+(defn new-tweet [username]
+  (let [text (r/atom nil)
+        save #(create-tweet username {:text @text})]
+    (fn []
+      [:form
+        [:div.form-group
+          [:label "Post new tweet"]
+          [:textarea.form-control {
+            :rows 5
+            :cols 80
+            :on-change #(reset! text (-> % .-target .-value))}]]
+          [:button.btn.btn-outline-success
+            {:on-click save}
+            "Post"]])))
+
 (defn user-page []
-  (let [username "user";(js/window -location -pathname)
-        routing-data (session/get :route)
+  (let [username (url/get-url-param)
         tweets (r/atom nil)]
-    (.log js/console username)
-    (.log js/console routing-data)
     (fetch-user-tweets username tweets)
     (fn []
         [:div.container
-        (for [t @tweets]
-          ^{:key (:id t)}
-          [tweet/tweet t])])))
+          [new-tweet username]
+          (for [t @tweets]
+            ^{:key (:id t)}
+            [tweet/tweet t])])))
