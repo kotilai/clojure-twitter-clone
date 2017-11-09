bd658fb9ef0ed0ccc8ed38033afb3393f07df844
Unknown <kotilainen.jussi@gmail.com>
2017-11-09 19:23:05 +0200

    Prettier tweet

--- src/cljs/clojure_twitter_clone/components/tweet.cljs
+++ src/cljs/clojure_twitter_clone/components/tweet.cljs
@@ -2,12 +2,31 @@
   (:require [reagent.format :as format]))
 
 (defn format-date [date]
-  (format/date-format date "dd.MM.yyyy HH.mm.ss"))
+  (format/date-format date "dd.MM.yyyy HH:mm:ss"))
+
+(defn date-padge [date]
+  (let [formatted (format-date date)]
+    [:span.date-padge formatted]))
+
+(defn username-padge [username]
+  (let [uri (str "#/" username)]
+    [:a.username-padge {:href uri}
+      [:strong username]]))
+
+(defn footer-link [class uri]
+  [:div.footer-link
+    [:a {:class class
+         :href uri}]])
 
 (defn tweet [content]
   (let [username (:username content)
-        date (format-date (:posted_date content))]
-  [:div.card
+        date (:posted_date content)]
+  [:div.tweet.card
     [:div.card-body
-      [:h5.card-title (str username " " date)]
-      [:p.card-text (:text content)]]]))
+      [:div.card-title
+        [username-padge username]
+        [date-padge date]]
+      [:p.card-text (:text content)]
+      [footer-link "fa fa-reply" "#"]
+      [footer-link "fa fa-refresh" "#"]
+      [footer-link "fa fa-heart-o" "#"]]]))
